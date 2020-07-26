c4f4d374615e2adf79584a1637de88e8064f7d6a
Thalles koester <thallesdella@gmail.com>
2020-07-26 17:07:56 -0300

    Auto config of smo

--- config/optimizer.php
+++ config/optimizer.php
@@ -1,7 +1,22 @@
 <?php
 
 return [
-    'name' => env('SITE_NAME', env('APP_NAME')),
-    'description' => env('SITE_DESC'),
-    'image' => env('SITE_IMAGE')
+    'site' => [
+        'name' => env('SITE_NAME', env('APP_NAME')),
+        'description' => env('SITE_DESC'),
+        'image' => env('SITE_IMAGE'),
+        'schema' => env('SITE_SCHEMA', 'article')
+    ],
+    'fb' => [
+        'auto' => env('FB_AUTO', false),
+        'page' => env('FB_PAGE', null),
+        'author' => env('FB_AUTHOR', null),
+        'id' => env('FB_ID', false),
+        'admins' => env('FB_ADMINS', false),
+    ],
+    'twitter' => [
+        'auto' => env('TWITTER_AUTO', false),
+        'creator' => env('TWITTER_CREATOR', null),
+        'card' => env('TWITTER_CARD', 'summary_large_image'),
+    ]
 ];
--- src/Providers/OptimizerServiceProvider.php
+++ src/Providers/OptimizerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace ThallesDella\Optimizer\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use ThallesDella\Optimizer\Optimizer;
 
@@ -9,7 +10,7 @@ class OptimizerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('Optimizer', function () {
+        $this->app->singleton('Optimizer', function () {
             return new Optimizer();
         });
         
@@ -23,5 +24,34 @@ class OptimizerServiceProvider extends ServiceProvider
         $this->publishes([
             dirname(__DIR__, 2) . '/config/optimizer.php' => config_path('optimizer.php'),
         ]);
+        
+        Optimizer::openGraph(
+            Config::get('optimizer.site.name', Config::get('app.name')),
+            $this->app->getLocale(),
+            Config::get('optimizer.site.schema', 'article')
+        );
+        
+        if (Config::get('optimizer.fb.auto', false)) {
+            Optimizer::publisher(
+                Config::get('optimizer.fb.page', null),
+                Config::get('optimizer.fb.author', null)
+            );
+            
+            if (Config::get('optimizer.fb.id', false)) {
+                Optimizer::facebook(Config::get('optimizer.fb.id'));
+            } elseif (Config::get('optimizer.fb.admins', false)) {
+                Optimizer::facebook(null, Config::get('optimizer.fb.admins'));
+            }
+        }
+        
+        if (Config::get('optimizer.twitter.auto', false)) {
+            Optimizer::twitterCard(
+                Config::get('optimizer.twitter.creator', null),
+                Config::get('optimizer.site.name', null),
+                Config::get('url', null),
+                Config::get('optimizer.twitter.card', 'summary_large_image')
+            );
+        }
+        
     }
 }
